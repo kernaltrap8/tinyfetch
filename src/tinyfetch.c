ef944a37a312e22fb820fcf3a50ff39b7e910391
kernaltrap8 <kernaltrap@gmail.com>
2024-04-13 15:05:49 -0500

    make `help_banner` a little more organized

--- src/tinyfetch.c
+++ src/tinyfetch.c
@@ -3,7 +3,11 @@
 #include <string.h>
 
 #define VERSION "0.5"
-#define help_banner "%s help\n -v or --version     print the installed version of tinyfetch\n -h or --help        print this help banner\n -a or --all         display all information\n -m or --message     add a custom message at the end of arguments\n"
+#define help_banner "%s help\n -v or --version     \
+	print the installed version of tinyfetch\n -h or --help        \
+	print this help banner\n -a or --all         \
+	display all information\n -m or --message     \
+	add a custom message at the end of arguments\n"
 
 void kernel_print(void) {
 	system("uname -o");

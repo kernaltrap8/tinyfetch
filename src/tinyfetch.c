e54fb533948f3d0e63ca5d12a88d1793f03dce8d
kernaltrap8 <kernaltrap@gmail.com>
2024-04-14 14:01:43 -0500

    add some more fetch features, make output more pretty

--- src/tinyfetch.c
+++ src/tinyfetch.c
@@ -2,23 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 
-#define VERSION "0.5"
+#define VERSION 	"0.7"
+#define decoration  "[·]"
 #define help_banner "%s help\n -v or --version     \
 	print the installed version of tinyfetch\n -h or --help        \
 	print this help banner\n -a or --all         \
 	display all information\n -m or --message     \
-	add a custom message at the end of arguments\n"
+	add a custom message at the end of arguments\n you can also combine args like \"%s -am \'message\' to get the full fetch, plus a custom message.\n"
+
+#define pretext_OS 		  "OS:         "
+#define pretext_kernel 	  "Kernel:     "
+#define pretext_arch 	  "Arch:       "
+#define pretext_kernver   "Kernel ver: "
+#define pretext_processor "CPU:        "
+#define pretext_shell	  "Shell:      "
+
+void pretext(char* string) {
+	printf(string);
+	fflush(stdout);
+}
 
 void kernel_print(void) {
+	pretext(pretext_OS);
 	system("uname -o");
+	pretext(pretext_kernel);
 	system("uname -r");
 }
 
 void print_all(void) {
+	char* shell = getenv("SHELL");
+	
 	kernel_print();
+	pretext(pretext_arch);
 	system("uname -m");
-	system("uname -v");
+	pretext(pretext_shell);
+	printf("%s\n", shell);
+	pretext(pretext_processor);
 	system("uname -p");
+	pretext(pretext_kernver);
+	system("uname -v");
 }
 
 int main(int argc, char* argv[]) {
@@ -31,16 +53,25 @@ int main(int argc, char* argv[]) {
 		printf("%s v%s", argv[0], VERSION);
 		return 0;
 	} if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
-		printf(help_banner, argv[0]);
+		printf(help_banner, argv[0], argv[0]);
 		return 0;
 	} if (!strcmp(argv[1], "-m") || !strcmp(argv[1], "--message")) {
 		if (argc < 3) {
 			printf("no message provided.\n");
 			return 1;
 		}
-		printf("%s", argv[2]);
+		fflush(stdout);
+		printf("%s  %s\n", decoration, argv[2]);
 		kernel_print();
 		return 0;
+	} if (!strcmp(argv[1], "-am")) {
+		if (argc < 3) {
+			printf("no message provided.\n");
+			return 1;
+		}
+		fflush(stdout);
+		printf("%s %s\n", decoration, argv[2]);
+		print_all();
 	} if (!strcmp(argv[1], "-a") || !strcmp(argv[1], "--all")) {
 			print_all();
 	}

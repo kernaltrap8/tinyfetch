b47f06ae480494f6eda0299b6a91d79517f5e5b0
kernaltrap8 <kernaltrap@gmail.com>
2024-04-18 18:44:23 -0500

    update version

--- src/tinyfetch.h
+++ src/tinyfetch.h
@@ -3,7 +3,7 @@
 	strings for tinyfetch
 */
 
-#define VERSION 	"2.1b"
+#define VERSION 	"2.3"
 #define decoration  "[·]"
 #define CMDLINE_PATH "/proc/%d/cmdline"
 #define help_banner "tinyfetch help\n -v or --version     \

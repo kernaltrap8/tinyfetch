2a61d69a196f3f2d9f987db9f307d0f0364f7ac2
kernaltrap8 <kernaltrap@gmail.com>
2024-04-29 15:55:58 -0500

    fix miscalculation of RAM used

--- src/tinyfetch.h
+++ src/tinyfetch.h
@@ -3,7 +3,7 @@
 	strings for tinyfetch
 */
 
-#define VERSION 	"3.1a"
+#define VERSION 	"3.1b"
 #define decoration  "[·]"
 #define CMDLINE_PATH "/proc/%d/cmdline"
 #define help_banner "tinyfetch help\n -v or --version     \

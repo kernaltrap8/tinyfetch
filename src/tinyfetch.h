d7dafb16035f4dabf2bae1e1dfc85484738e8286
kernaltrap <kernaltrap@gmail.com>
2024-08-10 14:03:44 -0500

    Update version
    Signed-off-by: kernaltrap <kernaltrap@gmail.com>

--- src/tinyfetch.h
+++ src/tinyfetch.h
@@ -6,7 +6,7 @@
 /*
     tinyfetch.h
 */
-#define VERSION "6.1"
+#define VERSION "6.2"
 #define decoration "[·]"
 #define CMDLINE_PATH "/proc/%d/cmdline"
 #define help_banner                                                            \
